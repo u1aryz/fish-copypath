d73506a0c01532aa357190b841fa24c327195bb1
u1aryz <u1aryz.d@gmail.com>
2026-08-03 03:12:54 +0900

    fix: support Fish 3.5 completions

--- completions/copypath.fish
+++ completions/copypath.fish
@@ -1,2 +1,2 @@
 complete --command copypath --no-files
-complete --command copypath --condition 'test (count (commandline --tokens-expanded --cut-at-cursor)) -eq 1' --force-files
+complete --command copypath --condition 'test (count (commandline --tokenize --cut-at-cursor --current-process)) -eq 1' --force-files
